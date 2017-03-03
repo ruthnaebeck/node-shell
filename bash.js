bb5da872023989237df8cfab123380aa830a9a7c
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-03-03 16:40:36 -0500

    Update for #2 The Process is Global

---
+++ bash.js
@@ -0,0 +1,20 @@
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var cmd = data.toString().trim(); // remove the newline
+
+  if(cmd === 'pwd'){
+    process.stdout.write(process.cwd());
+  }else if(cmd === 'date'){
+    var today = new Date();
+    var GMTstring = today.toGMTString();
+    process.stdout.write(GMTstring);
+  }else{
+    process.stdout.write('You typed: ' + cmd);
+  }
+
+  process.stdout.write('\nprompt > ');
+
+});
