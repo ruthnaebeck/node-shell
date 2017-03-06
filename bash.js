368d5bbfdf5d0298a8332547e2c9bd8d6082f2d7
Cigdem Aybar <aybarcigdem@gmail.com>
2017-03-06 11:43:47 -0500

    monday morning

--- bash.js
+++ bash.js
@@ -3,22 +3,20 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-
-  if(cmd === 'pwd'){
-    process.stdout.write(process.cwd());
-  }else if(cmd === 'date'){
-    var today = new Date();
-    var GMTstring = today.toGMTString();
-    process.stdout.write(GMTstring);
-  }else{
-    process.stdout.write('You typed: ' + cmd);
-  }
+  var dataArr = data.toString().trim().split(" ");
+  var cmd = dataArr[0]
+  var commands = require('./command');
+   commands[cmd](dataArr);
+  // if(cmd === 'pwd' || cmd === 'ls' || cmd === 'echo'){
+  //   commands[cmd](dataArr);
+  // }else if(cmd === 'date'){
+  //   var today = new Date();
+  //   var GMTstring = today.toGMTString();
+  //   process.stdout.write(GMTstring);
+  // } else{
+  //   process.stdout.write('You typed: ' + cmd);
+  // }
 
   process.stdout.write('\nprompt > ');
 
 });
-
-var commands = require('./commands');
-var userCommand = 'pwd';
-commands[userCommand]();
--- command.js
+++ command.js
@@ -1,5 +1,50 @@
+var fs = require('fs');
+
 module.exports = {
 	pwd: function(){
 		process.stdout.write(process.cwd());
-	}
+		process.stdout.write('\nprompt > ');
+	},
+
+	ls : function(){
+		fs.readdir('.', function(err, files) {
+  	if (err) throw err;
+  	files.forEach(function(file) {
+    process.stdout.write(file.toString() + "\n");
+  	})
+  	process.stdout.write("prompt > ");
+		});
+	},
+
+	echo : function(arg){
+		process.stdout.write(arg.slice(1).join(" "));
+	},
+
+	cat : function(filename){
+		fs.readFile(filename[1], (err, data) => {
+  	if (err) throw err;
+  	process.stdout.write(data);
+		});
+	},
+
+	head : function(filename){
+		fs.readFile(filename[1], (err, data) => {
+  	if (err) throw err;
+		var counter = 0;
+		var headString = '';
+		var dataString = data.toString();
+		for(var i = 0; i < dataString.length; i++){
+			if(dataString[i]=== '\n') {
+				counter++;
+			}
+			headString += dataString[i];
+			if(counter >= 5){
+				break;
+			}
+		}
+		process.stdout.write(headString);
+		});
+	},
 }
+
+
